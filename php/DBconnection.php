fdf8af83ac1879a25dc28e4c4e41f86ccc5cc1c3
Ediz Göldeli <38820143+Edizgoldeli@users.noreply.github.com>
2022-11-12 11:42:14 +0300

    Update DBconnection.php

--- php/DBconnection.php
+++ php/DBconnection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password="root";
-$dbname = "goldeliMarket";
+$dbname = "root";
 $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->set_charset("utf8");
 if ($conn->connect_error) {
